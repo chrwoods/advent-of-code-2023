6a47d8c00af7ecd5ae1a875bc90365e01424f8b2
Christopher Woods <chrisscottwoods@gmail.com>
2023-12-25 00:22:26 -0600

    p9 actual commit

---
+++ problems/p9.py
@@ -0,0 +1,104 @@
+import bisect
+import collections
+import functools
+import math
+from os import truncate
+import regex as re
+from collections import defaultdict as dd
+from z3 import *
+
+DIGITS_CHARS = '0123456789'
+
+
+def word_to_num(word):
+    map = {
+        'zero': 0,
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+    }
+    return str(map.get(word, word))
+
+
+def make_empty_grid(factory, dimensions, *sizes):
+    if dimensions == 0:
+        return factory()
+    return [
+make_empty_grid(factory, dimensions - 1, *(sizes[1:]))
+for _ in range(sizes[0])
+]
+
+
+def get_nums(a, split_char=' '):
+    return [int(x.strip()) for x in a.split(split_char)]
+
+
+# find leftmost true (assumming F, F, T, T)
+def bin_search(test, a, b):
+    while a < b:
+        m = (a + b) // 2
+    if test(m):
+        b = m
+    else:
+        a = m + 1
+    return a
+
+
+def transpose(grid):
+    return list(zip(*grid))
+
+
+def distt(a, b):
+    return abs(b[1] - a[1]) + abs(b[0] - a[0])
+
+
+def rotate_grid_clockwise(grid):
+    return transpose(grid[::-1])
+
+
+def print_grid(grid):
+    for row in grid:
+        print(''.join(row))
+
+
+def print_bool_grid(grid):
+    for row in grid:
+        print(''.join('#' if x else '.' for x in row))
+
+
+def dims(grid):
+    return len(grid), len(grid[0])
+
+
+d4 = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+file = open("input/t9.txt", "r")
+score = 0
+grid = []
+
+def get_next(seq):
+    if len(set(seq)) == 1:
+        return seq[0]
+    add = get_next([b - a for a, b in zip(seq, seq[1:])])
+    return seq[-1] + add
+
+def get_prev(seq):
+    if len(set(seq)) == 1:
+        return seq[0]
+    add = get_prev([b - a for a, b in zip(seq, seq[1:])])
+    return seq[0] - add
+
+
+for idx, line in enumerate(file.readlines()):
+    line = line.strip()
+    seq = get_nums(line)
+    score += get_prev(seq)
+    
+    
+print(score)
